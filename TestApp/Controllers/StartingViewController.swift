e5e7e87c0d29ea5a13f29270ab9e18b28490fd6d
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-07-08 16:17:00 +0200

    Internet reachability monitoring on

--- TestApp/Controllers/StartingViewController.swift
+++ TestApp/Controllers/StartingViewController.swift
@@ -14,6 +14,7 @@ class StartingViewController: UIViewController {
         super.viewDidAppear(animated)
         
         getData()
+        ReachabilityManager.shared.startMonitoring()
     }
     
     private func getData() {
